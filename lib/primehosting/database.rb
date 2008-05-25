f90e369a0a978e62e1c96952c7256b1e0be3868d
Geoff Garside <geoff@geoffgarside.co.uk>
2008-05-25 01:56:49 +0100

    Make database:configure prompt for username and password

--- lib/primehosting/database.rb
+++ lib/primehosting/database.rb
@@ -1,18 +1,31 @@
+require 'highline/import'
+
 Capistrano::Configuration.instance(true).load do
   set :database_name, nil
-  set :database_user, nil
+  set :database_user, Proc.new { Highline.ask("What is your database username?  ") { |q| q.default = "dbuser" } }
+  set :database_pass, Proc.new { Highline.ask("What is your database password?  ") { |q| q.echo = "*" } }
   
   namespace :database do
     task :configure do
-      db_config = {
-        "production" => {
-          "adapter" => "mysql",
-          "database" => database_name,
-          "username" => database_user,
-          "password" => database_pass,
-          "host" => "localhost"
-        }
+      db_config =<<EOF
+production:
+  adapter: mysql
+  database: #{database_name}
+  username: #{database_user}
+  password: #{database_pass}
+  host: 127.0.0.1
+EOF
+      run "if test ! -d #{shared_path}/config/; then mkdir -p #{shared_path}/config/; fi"
+      put db_config, "#{shared_path}/config/database.yml"
+    end
+    
+    task :copy_config do
+      on_rollback {
+        puts "*** File shared/config/database.yml is missing. ***"
+        puts "*** Run cap database:configure to generate and upload. ***"
       }
+      
+      run "cp #{shared_path}/config/database.yml #{release_path}/config/"
     end
   end
 end
